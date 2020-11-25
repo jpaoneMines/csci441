bdcb2b30cdce8cbdf70a700adff8116cba3da0c7
jpaoneMines <jpaone@mines.edu>
2020-11-25 13:55:33 -0700

    Print error messages to error stream. Added interface to check status message for a specific FBO handle, instead of just the currently bound FBO.

--- FramebufferUtils.hpp
+++ FramebufferUtils.hpp
@@ -28,8 +28,23 @@ namespace CSCI441 {
 	  */
 	namespace FramebufferUtils {
 
+	    /**
+	     * Prints the framebuffer information for the FBO attached to the corresponding target
+	     * @param target
+	     * @param fbo
+	     */
 		void printFramebufferInfo( GLenum target, GLuint fbo );
 
+		/**
+		 * Prints the framebuffer status for the FBO attached to the corresponding target
+		 * @param target
+		 * @param fbo
+		 */
+        void printFramebufferStatusMessage( GLenum target, GLuint fbo );
+        /**
+         * Prints the framebuffer status for the FBO currently attached to the correspondign target
+         * @param target
+         */
 		void printFramebufferStatusMessage( GLenum target );
 	}
 }
@@ -42,19 +57,32 @@ inline void CSCI441::FramebufferUtils::printFramebufferInfo( GLenum target, GLui
 	int res, i = 0;
 	GLint buffer;
 
-	glBindFramebuffer(target,fbo);
+	if(glIsFramebuffer(fbo)) {
+        glBindFramebuffer( target, fbo );
 
-	do {
-        glGetIntegerv(GL_DRAW_BUFFER0+i, &buffer);
-        if (buffer != GL_NONE) {
-            printf("[FBO]: Shader Output Location %d -> color attachment %d\n", i, buffer - GL_COLOR_ATTACHMENT0);
-            glGetFramebufferAttachmentParameteriv(target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE, &res);
-            printf("[FBO]:  \tAttachment Type: %s\n", res == GL_TEXTURE ? "Texture" : "Render Buffer" );
-            glGetFramebufferAttachmentParameteriv(target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &res);
-            printf("[FBO]:  \tAttachment object name: %d\n",res);
-        }
-        ++i;
-	} while (buffer != GL_NONE);
+        do {
+            glGetIntegerv( GL_DRAW_BUFFER0 + i, &buffer );
+            if( buffer != GL_NONE ) {
+                printf( "[FBO]: Shader Output Location %d -> color attachment %d\n", i, buffer - GL_COLOR_ATTACHMENT0 );
+                glGetFramebufferAttachmentParameteriv( target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE, &res );
+                printf( "[FBO]:  \tAttachment Type: %s\n", res == GL_TEXTURE ? "Texture" : "Render Buffer" );
+                glGetFramebufferAttachmentParameteriv( target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &res );
+                printf( "[FBO]:  \tAttachment object name: %d\n", res );
+            }
+            ++i;
+        } while( buffer != GL_NONE );
+    } else {
+	    fprintf(stderr, "[FBO]: Error: %d is not a framebuffer\n", fbo);
+	}
+}
+
+inline void CSCI441::FramebufferUtils::printFramebufferStatusMessage( GLenum target, GLuint fbo ) {
+    if(glIsFramebuffer(fbo)) {
+        glBindFramebuffer( target, fbo );
+        printFramebufferStatusMessage(target);
+    } else {
+        fprintf(stderr, "[FBO]: Error: %d is not a framebuffer\n", fbo);
+    }
 }
 
 inline void CSCI441::FramebufferUtils::printFramebufferStatusMessage( GLenum target ) {
@@ -62,49 +90,49 @@ inline void CSCI441::FramebufferUtils::printFramebufferStatusMessage( GLenum tar
 	if( status == GL_FRAMEBUFFER_COMPLETE )
 		printf( "[FBO]: Framebuffer initialized completely!\n" );
 	else {
-		printf( "[ERROR]: Framebuffer failed to initialize completely 0x%x.\n", status );
+		fprintf( stderr, "[FBO]: Framebuffer failed to initialize completely 0x%x.\n", status );
 		switch(status) {
 		case GL_FRAMEBUFFER_COMPLETE:
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-		  printf("An attachment could not be bound to framebuffer object!\n");
+          fprintf(stderr, "[FBO]: An attachment could not be bound to framebuffer object!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-		  printf("Attachments are missing! At least one image (texture) must be bound to the framebuffer object!\n");
+		  fprintf(stderr, "[FBO]: Attachments are missing! At least one image (texture) must be bound to the framebuffer object!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
-		  printf("The dimensions of the buffers attached to the currently used framebuffer object do not match!\n");
+		  fprintf(stderr, "[FBO]: The dimensions of the buffers attached to the currently used framebuffer object do not match!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT:
-		  printf("The formats of the currently used framebuffer object are not supported or do not fit together!\n");
+		  fprintf(stderr, "[FBO]: The formats of the currently used framebuffer object are not supported or do not fit together!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
-		  printf("A Draw buffer is incomplete or undefined. All draw buffers must specify attachment points that have images attached.\n");
+		  fprintf(stderr, "[FBO]: A Draw buffer is incomplete or undefined. All draw buffers must specify attachment points that have images attached.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
-		  printf("A Read buffer is incomplete or undefined. All read buffers must specify attachment points that have images attached.\n");
+		  fprintf(stderr, "[FBO]: A Read buffer is incomplete or undefined. All read buffers must specify attachment points that have images attached.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-		  printf("All images must have the same number of multisample samples.\n");
+		  fprintf(stderr, "[FBO]: All images must have the same number of multisample samples.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS :
-		  printf("If a layered image is attached to one attachment, then all attachments must be layered attachments. The attached layers do not have to have the same number of layers, nor do the layers have to come from the same kind of texture.\n");
+		  fprintf(stderr, "[FBO]: If a layered image is attached to one attachment, then all attachments must be layered attachments. The attached layers do not have to have the same number of layers, nor do the layers have to come from the same kind of texture.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_UNSUPPORTED:
-		  printf("Attempt to use an unsupported format combination!\n");
+		  fprintf(stderr, "[FBO]: Attempt to use an unsupported format combination!\n");
 		  break;
 
 		default:
-		  printf("Unknown error while attempting to create framebuffer object!\n");
+		  fprintf(stderr, "[FBO]: Unknown error while attempting to create framebuffer object!\n");
 		  break;
 		}
 	}
