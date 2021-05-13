3439bc113734d4c9ebf3eb5189508b002c8f9bd8
jpaone <jpaone@mines.edu>
2021-05-13 09:07:58 -0600

    Added new files to include all

--- CSCI441.hpp
+++ CSCI441.hpp
@@ -1,8 +1,6 @@
 /** @file CSCI441.hpp
  * @brief Includes all CSCI 441 class helper files
  * @author Dr. Jeffrey Paone
- * @date Last Edit: 08 Jun 2020
- * @version 2.0
  *
  * @copyright MIT License Copyright (c) 2017 Dr. Jeffrey Paone
  *
@@ -16,11 +14,14 @@
 #ifndef CSCI441_CSCI441_H
 #define CSCI441_CSCI441_H
 
+#include "ComputeShaderProgram.hpp" // helper class to compile and use compute shaders
 #include "FramebufferUtils.hpp"     // to query common FBO information
+#include "materials.hpp"            // predefined material properties
 #include "modelLoader.hpp"          // to load OBJ, OFF, PLY, STL files
 #include "OpenGLUtils.hpp"          // to query OpenGL features
 #include "objects.hpp"              // include 3D objects (cube, cylinder, cone, torus, sphere, disk, teapot)
 #include "ShaderProgram.hpp"        // helper class to compile and use shaders
+#include "ShaderProgramPipeline.hpp"// helper class to compile and use shader program pipelines
 #include "TextureUtils.hpp"         // helper functions for registering textures
 
 #endif //CSCI441_CSCI441_H
