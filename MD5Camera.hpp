598fe95642e4e43f56514be71278d70bbd3781e6
jpaone <jpaone@mines.edu>
2023-08-30 15:07:54 -0600

    Issue #13 - began file parsing

--- MD5Camera.hpp
+++ MD5Camera.hpp
@@ -3,6 +3,10 @@
 
 #include "Camera.hpp"
 
+#include <ctime>
+#include <fstream>
+#include <string>
+
 namespace CSCI441 {
 
     /**
@@ -18,15 +22,19 @@ namespace CSCI441 {
          * @param aspectRatio aspect ratio of view plane (defaults to 1.0f)
          * @param nearClipPlane near z clip plane (defaults to 0.001f)
          * @param farClipPlane far z clip plane (defaults to 1000.0f)
+         * @param INFO if file loading information should be printed to standard out (defaults to true)
+         * @param ERRORS if file loading errors should be printed to standard error (defaults to true)
          * @note field of view specified in degrees
          */
-        MD5Camera(const char* MD5CAMERA_FILE, GLfloat fovy = 45.0f, GLfloat aspectRatio = 1.0f, GLfloat nearClipPlane = 0.001f, GLfloat farClipPlane = 1000.0f);
+        MD5Camera(const char* MD5CAMERA_FILE, GLfloat fovy = 45.0f, GLfloat aspectRatio = 1.0f, GLfloat nearClipPlane = 0.001f, GLfloat farClipPlane = 1000.0f, GLboolean INFO = true, GLboolean ERRORS = true);
 
         void recomputeOrientation() final {}
         void moveForward(GLfloat unused) final;
         void moveBackward(GLfloat unused) final;
 
     private:
+        bool _loadMD5CameraFromFile(const char * MD5CAMERA_FILE, GLboolean INFO = true, GLboolean ERRORS = true);
+
         struct Frame {
             glm::vec3 cameraPosition;
             glm::vec3 cameraQuaternion;
@@ -56,7 +64,9 @@ inline CSCI441::MD5Camera::MD5Camera(
         const GLfloat fovy,
         const GLfloat aspectRatio,
         const GLfloat nearClipPlane,
-        const GLfloat farClipPlane
+        const GLfloat farClipPlane,
+        const GLboolean INFO,
+        const GLboolean ERRORS
 ) : _fovy(fovy),
     _aspectRatio(aspectRatio),
     _nearClipPlane(nearClipPlane),
@@ -65,6 +75,68 @@ inline CSCI441::MD5Camera::MD5Camera(
     _currentFrameIndex = 0;
     _currentCutIndex = 0;
     mProjectionMatrix = glm::perspective(_fovy, _aspectRatio, _nearClipPlane, _farClipPlane);
+
+    _loadMD5CameraFromFile(MD5CAMERA_FILE, INFO, ERRORS);
+}
+
+inline bool CSCI441::MD5Camera::_loadMD5CameraFromFile(
+        const char * const MD5CAMERA_FILE,
+        const GLboolean INFO,
+        const GLboolean ERRORS
+) {
+    if ( INFO ) fprintf( stdout, "[.md5camera]: -=-=-=-=-=-=-=- BEGIN %s Info -=-=-=-=-=-=-=- \n", MD5CAMERA_FILE );
+
+    time_t start, end;
+    time(&start);
+
+    std::ifstream md5CameraFile(MD5CAMERA_FILE);
+    if( !md5CameraFile.is_open() ) {
+        if (ERRORS) fprintf( stderr, "[.md5camera]: [ERROR]: Could not open \"%s\"\n", MD5CAMERA_FILE );
+        if ( INFO ) fprintf( stdout, "[.md5camera]: -=-=-=-=-=-=-=-  END  %s Info -=-=-=-=-=-=-=- \n", MD5CAMERA_FILE );
+        return false;
+    }
+
+    std::string md5versionStr;
+    int md5version;
+    md5CameraFile >> md5versionStr >> md5version;
+    if(md5versionStr != "MD5Version" || md5version != 10) {
+        if (ERRORS) fprintf( stderr, "[.md5camera]: [ERROR]: improper MD5Camera version found \"%s %d\"\n", md5versionStr.c_str(), md5version );
+        if ( INFO ) fprintf( stdout, "[.md5camera]: -=-=-=-=-=-=-=-  END  %s Info -=-=-=-=-=-=-=- \n", MD5CAMERA_FILE );
+        return false;
+    }
+
+    // TODO read - commandline "string"
+
+    // TODO read - numFrames <integer>
+    // TODO read - frameRate <integer>
+    // TODO read - numCuts <integer>
+
+    // TODO sections
+    // cuts {
+    //   [frameNumber]
+    //   [frameNumber]
+    // }
+
+    // camera {
+    //   ( [x] [y] [z] ) ( [orientation] ) [FOV]
+    // }
+
+    if (INFO) {
+        printf( "[.md5camera]: Camera Stats:\n" );
+        printf( "[.md5camera]: Num Frames:\t%u\n", _numFrames );
+        printf( "[.md5camera]: Frame Rate:\t%u\n", _frameRate );
+        printf( "[.md5camera]: Num Cuts:  \t%u\n", _numCuts );
+    }
+
+    time(&end);
+    double seconds = difftime( end, start );
+
+    if (INFO) {
+        printf( "[.md5camera]: Completed in %.3fs\n", seconds );
+        printf( "[.md5camera]: -=-=-=-=-=-=-=-  END %s Info  -=-=-=-=-=-=-=- \n\n", MD5CAMERA_FILE );
+    }
+
+    return true;
 }
 
 inline void CSCI441::MD5Camera::moveForward(const GLfloat unused) {
