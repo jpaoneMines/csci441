a648a4518a45bdd8be68ce7b8b24efab9932ae96
jpaone <jpaone@mines.edu>
2025-09-04 10:51:51 -0600

    query max atomic counter buffer size

--- OpenGLUtils.hpp
+++ OpenGLUtils.hpp
@@ -162,7 +162,7 @@ inline void CSCI441::OpenGLUtils::printOpenGLInfo() {
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Attributes:  %26d |\n", 					    	GL_MAX_VERTEX_ATTRIBS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Uniforms:  %28d |\n", 							GL_MAX_VERTEX_UNIFORM_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Textures:  %28d |\n", 							GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Outputs:  %29d |\n", 						    GL_MAX_VERTEX_OUTPUT_COMPONENTS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Outputs:  %29d |\n", 						    	GL_MAX_VERTEX_OUTPUT_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Inputs:  %28d |\n", 							GL_MAX_FRAGMENT_INPUT_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Uniforms:  %26d |\n", 					    	GL_MAX_FRAGMENT_UNIFORM_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Textures:  %26d |\n", 					    	GL_MAX_TEXTURE_IMAGE_UNITS );
@@ -172,7 +172,7 @@ inline void CSCI441::OpenGLUtils::printOpenGLInfo() {
 
 	if( (major >= 3 && minor >= 0) || major > 3 ) {
 		CSCI441_INTERNAL::printOpenGLParamHeader( 3, 0 );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Transform Feedback Buffers:  %17d |\n", 			    GL_MAX_TRANSFORM_FEEDBACK_BUFFERS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Transform Feedback Buffers:  %17d |\n", 			    	GL_MAX_TRANSFORM_FEEDBACK_BUFFERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Transform Separate Attributes:  %14d |\n", 		    	GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_ATTRIBS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Transform Separate Components:  %14d |\n", 		    	GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Transform Interleaved Buffers:  %14d |\n", 		    	GL_MAX_TRANSFORM_FEEDBACK_INTERLEAVED_COMPONENTS );
@@ -200,8 +200,8 @@ inline void CSCI441::OpenGLUtils::printOpenGLInfo() {
 
 	if( (major >= 4 && minor >= 0) || major > 4 ) {
 		CSCI441_INTERNAL::printOpenGLParamHeader( 4, 0 );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Patch Vertices:  %29d |\n", 						    GL_MAX_PATCH_VERTICES );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tessellation Level:  %25d |\n", 					    GL_MAX_TESS_GEN_LEVEL );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Patch Vertices:  %29d |\n", 						    	GL_MAX_PATCH_VERTICES );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tessellation Level:  %25d |\n", 					    	GL_MAX_TESS_GEN_LEVEL );
 		CSCI441_INTERNAL::printOpenGLParam4("[INFO]: |   Default Tessellation Outer Levels:  %7d %2d %2d %2d |\n",   	GL_PATCH_DEFAULT_OUTER_LEVEL );
 		CSCI441_INTERNAL::printOpenGLParam2("[INFO]: |   Default Tessellation Inner Levels:  %13d %2d |\n", 			GL_PATCH_DEFAULT_INNER_LEVEL );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Inputs:  %24d |\n", 						GL_MAX_GEOMETRY_INPUT_COMPONENTS );
@@ -209,45 +209,46 @@ inline void CSCI441::OpenGLUtils::printOpenGLInfo() {
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Uniform Blocks:  %16d |\n", 				GL_MAX_TESS_CONTROL_UNIFORM_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Textures:  %22d |\n", 					    GL_MAX_TESS_CONTROL_TEXTURE_IMAGE_UNITS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Outputs:  %23d |\n", 						GL_MAX_TESS_CONTROL_OUTPUT_COMPONENTS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Inputs:  %21d |\n", 					GL_MAX_TESS_EVALUATION_INPUT_COMPONENTS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Inputs:  %21d |\n", 						GL_MAX_TESS_EVALUATION_INPUT_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Uniforms:  %19d |\n", 					GL_MAX_TESS_EVALUATION_UNIFORM_COMPONENTS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Uniform Blocks:  %13d |\n", 		    GL_MAX_TESS_EVALUATION_UNIFORM_BLOCKS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Uniform Blocks:  %13d |\n", 		    	GL_MAX_TESS_EVALUATION_UNIFORM_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Textures:  %19d |\n", 					GL_MAX_TESS_EVALUATION_TEXTURE_IMAGE_UNITS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Outputs:  %20d |\n", 					GL_MAX_TESS_EVALUATION_OUTPUT_COMPONENTS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Invocations:  %23d |\n", 				    	GL_MAX_GEOMETRY_SHADER_INVOCATIONS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Streams:  %29d |\n", 						    GL_MAX_VERTEX_STREAMS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Streams:  %29d |\n", 						    	GL_MAX_VERTEX_STREAMS );
 	}
 
 	if( (major >= 4 && minor >= 2) || major > 4 ) {
 		CSCI441_INTERNAL::printOpenGLParamHeader( 4, 2 );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Atomic Counters:  %21d |\n", 				    GL_MAX_VERTEX_ATOMIC_COUNTERS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Atomic Counters:  %21d |\n", 				    	GL_MAX_VERTEX_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Atomic Counters:  %15d |\n", 				GL_MAX_TESS_CONTROL_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Atomic Counters:  %12d |\n", 			GL_MAX_TESS_EVALUATION_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Atomic Counters:  %19d |\n", 					GL_MAX_GEOMETRY_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Atomic Counters:  %19d |\n", 					GL_MAX_FRAGMENT_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Atomic Counters:  %19d |\n", 					GL_MAX_COMBINED_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Atomic Counter Buffers:  %14d |\n", 		    	GL_MAX_VERTEX_ATOMIC_COUNTER_BUFFERS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Atomic Counter Buffers:  %8d |\n", 	    GL_MAX_TESS_CONTROL_ATOMIC_COUNTER_BUFFERS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Atomic Counter Buffers:  %8d |\n", 	    	GL_MAX_TESS_CONTROL_ATOMIC_COUNTER_BUFFERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Atomic Counter Buffers:  %5d |\n",   	GL_MAX_TESS_EVALUATION_ATOMIC_COUNTER_BUFFERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Atomic Counter Buffers:  %12d |\n", 			GL_MAX_GEOMETRY_ATOMIC_COUNTER_BUFFERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Atomic Counter Buffers:  %12d |\n", 			GL_MAX_FRAGMENT_ATOMIC_COUNTER_BUFFERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Atomic Counter Buffers:  %12d |\n", 			GL_MAX_COMBINED_ATOMIC_COUNTER_BUFFERS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Atomic Counter Buffer Bindings:  %13d |\n", 		    GL_MAX_ATOMIC_COUNTER_BUFFER_BINDINGS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Atomic Counter Buffer Bindings:  %13d |\n", 		    	GL_MAX_ATOMIC_COUNTER_BUFFER_BINDINGS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max Atomic Counter Buffer Size:  %17d |\n", 		    		GL_MAX_ATOMIC_COUNTER_BUFFER_SIZE );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Image Uniforms:  %22d |\n", 				    	GL_MAX_VERTEX_IMAGE_UNIFORMS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Image Uniforms:  %16d |\n", 				GL_MAX_TESS_CONTROL_IMAGE_UNIFORMS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Image Uniforms:  %13d |\n", 		    GL_MAX_TESS_EVALUATION_IMAGE_UNIFORMS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Image Uniforms:  %13d |\n", 		    	GL_MAX_TESS_EVALUATION_IMAGE_UNIFORMS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Image Uniforms:  %20d |\n", 					GL_MAX_GEOMETRY_IMAGE_UNIFORMS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Image Uniforms:  %20d |\n", 					GL_MAX_FRAGMENT_IMAGE_UNIFORMS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Image Uniforms:  %20d |\n", 					GL_MAX_IMAGE_UNITS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Shader Storage Buffer Bindings:  %13d |\n", 		    GL_MAX_SHADER_STORAGE_BUFFER_BINDINGS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Shader Storage Buffer Bindings:  %13d |\n", 		    	GL_MAX_SHADER_STORAGE_BUFFER_BINDINGS );
 	}
 
 	if( (major >= 4 && minor >= 3) || major > 4 ) {
 		CSCI441_INTERNAL::printOpenGLParamHeader( 4, 3 );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Uniforms:  %27d |\n", 							GL_MAX_COMPUTE_UNIFORM_COMPONENTS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Uniform Blocks:  %21d |\n", 					GL_MAX_COMPUTE_UNIFORM_BLOCKS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Uniform Blocks:  %21d |\n", 						GL_MAX_COMPUTE_UNIFORM_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Textures:  %27d |\n", 							GL_MAX_COMPUTE_TEXTURE_IMAGE_UNITS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Image Uniforms:  %21d |\n", 					GL_MAX_COMPUTE_IMAGE_UNIFORMS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Image Uniforms:  %21d |\n", 						GL_MAX_COMPUTE_IMAGE_UNIFORMS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Atomic Counters:  %20d |\n", 					GL_MAX_COMPUTE_ATOMIC_COUNTERS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Atomic Counter Buffers:  %13d |\n",				GL_MAX_COMPUTE_ATOMIC_COUNTER_BUFFERS );
 
@@ -264,10 +265,10 @@ inline void CSCI441::OpenGLUtils::printOpenGLInfo() {
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Vertex Shader Storage Blocks:  %15d |\n", 				GL_MAX_VERTEX_SHADER_STORAGE_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Control Shader Storage Blocks:  %9d |\n", 			GL_MAX_TESS_CONTROL_SHADER_STORAGE_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Tess Evaluation Shader Storage Blocks:  %6d |\n", 		GL_MAX_TESS_EVALUATION_SHADER_STORAGE_BLOCKS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Shader Storage Blocks:  %13d |\n", 			GL_MAX_GEOMETRY_SHADER_STORAGE_BLOCKS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Shader Storage Blocks:  %13d |\n", 			GL_MAX_FRAGMENT_SHADER_STORAGE_BLOCKS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Geometry Shader Storage Blocks:  %13d |\n", 				GL_MAX_GEOMETRY_SHADER_STORAGE_BLOCKS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Fragment Shader Storage Blocks:  %13d |\n", 				GL_MAX_FRAGMENT_SHADER_STORAGE_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Compute Shader Storage Blocks:  %14d |\n", 				GL_MAX_COMPUTE_SHADER_STORAGE_BLOCKS );
-		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Shader Storage Blocks:  %13d |\n", 			GL_MAX_COMBINED_SHADER_STORAGE_BLOCKS );
+		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Shader Storage Blocks:  %13d |\n", 				GL_MAX_COMBINED_SHADER_STORAGE_BLOCKS );
 		CSCI441_INTERNAL::printOpenGLParam( "[INFO]: |   Max # Combined Shader Output Resources:  %11d |\n", 			GL_MAX_COMBINED_SHADER_OUTPUT_RESOURCES );
 	}
 
@@ -290,7 +291,6 @@ inline void CSCI441::OpenGLUtils::checkOpenGLErrors() {
     GLenum err;
     while( (err = glGetError()) != GL_NO_ERROR ) {
         fprintf( stderr, "[ERROR]: OpenGL Error (%d): %s\n", err, CSCI441::OpenGLUtils::openGLErrorMessage(err) );
-        err = glGetError();
     }
 }
 
