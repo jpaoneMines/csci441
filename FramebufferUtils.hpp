e0c7615fedbe4aa92e5f478495fd93d5424e1964
jpaone <jpaone@mines.edu>
2026-05-21 21:18:00 -0600

    Replace fprintf with LogUtils

--- FramebufferUtils.hpp
+++ FramebufferUtils.hpp
@@ -15,6 +15,7 @@
 #define CSCI441_FRAMEBUFFER_UTILS_HPP
 
 #include "constants.h"
+#include "LogUtils.hpp"
 
 #ifdef CSCI441_USE_GLEW
     #include <GL/glew.h>
@@ -71,16 +72,16 @@ inline void CSCI441::FramebufferUtils::printFramebufferInfo( const GLenum target
         do {
             glGetIntegerv( GL_DRAW_BUFFER0 + i, &buffer );
             if( buffer != GL_NONE ) {
-                printf( "[FBO]: Shader Output Location %d -> color attachment %d\n", i, buffer - GL_COLOR_ATTACHMENT0 );
+                CSCI441::LogUtils::log("[FBO]: Shader Output Location %d -> color attachment %d\n", i, buffer - GL_COLOR_ATTACHMENT0 );
                 glGetFramebufferAttachmentParameteriv( target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE, &res );
-                printf( "[FBO]:  \tAttachment Type: %s\n", res == GL_TEXTURE ? "Texture" : "Render Buffer" );
+                CSCI441::LogUtils::log("[FBO]:  \tAttachment Type: %s\n", res == GL_TEXTURE ? "Texture" : "Render Buffer" );
                 glGetFramebufferAttachmentParameteriv( target, buffer, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &res );
-                printf( "[FBO]:  \tAttachment object name: %d\n", res );
+                CSCI441::LogUtils::log("[FBO]:  \tAttachment object name: %d\n", res );
             }
             ++i;
         } while( buffer != GL_NONE );
     } else {
-	    fprintf(stderr, "[FBO]: Error: %d is not a framebuffer\n", fbo);
+	    CSCI441::LogUtils::logError("[FBO]: Error: %d is not a framebuffer\n", fbo);
 	}
 }
 
@@ -90,56 +91,56 @@ inline void CSCI441::FramebufferUtils::printFramebufferStatusMessage( const GLen
         glBindFramebuffer( target, fbo );
         printFramebufferStatusMessage(target);
     } else {
-        fprintf(stderr, "[FBO]: Error: %d is not a framebuffer\n", fbo);
+        CSCI441::LogUtils::logError("[FBO]: Error: %d is not a framebuffer\n", fbo);
     }
 }
 
 inline void CSCI441::FramebufferUtils::printFramebufferStatusMessage( const GLenum target ) {
 	GLenum status = glCheckFramebufferStatus( target );
 	if( status == GL_FRAMEBUFFER_COMPLETE )
-		printf( "[FBO]: Framebuffer initialized completely!\n" );
+		CSCI441::LogUtils::log("[FBO]: Framebuffer initialized completely!\n" );
 	else {
-		fprintf( stderr, "[FBO]: Framebuffer failed to initialize completely 0x%x.\n", status );
+		CSCI441::LogUtils::logError("[FBO]: Framebuffer failed to initialize completely 0x%x.\n", status );
 
 		switch(status) {
 		case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-          fprintf(stderr, "[FBO]: An attachment could not be bound to framebuffer object!\n");
+          CSCI441::LogUtils::logError("[FBO]: An attachment could not be bound to framebuffer object!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-		  fprintf(stderr, "[FBO]: Attachments are missing! At least one image (texture) must be bound to the framebuffer object!\n");
+		  CSCI441::LogUtils::logError("[FBO]: Attachments are missing! At least one image (texture) must be bound to the framebuffer object!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
-		  fprintf(stderr, "[FBO]: The dimensions of the buffers attached to the currently used framebuffer object do not match!\n");
+		  CSCI441::LogUtils::logError("[FBO]: The dimensions of the buffers attached to the currently used framebuffer object do not match!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT:
-		  fprintf(stderr, "[FBO]: The formats of the currently used framebuffer object are not supported or do not fit together!\n");
+		  CSCI441::LogUtils::logError("[FBO]: The formats of the currently used framebuffer object are not supported or do not fit together!\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
-		  fprintf(stderr, "[FBO]: A Draw buffer is incomplete or undefined. All draw buffers must specify attachment points that have images attached.\n");
+		  CSCI441::LogUtils::logError("[FBO]: A Draw buffer is incomplete or undefined. All draw buffers must specify attachment points that have images attached.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
-		  fprintf(stderr, "[FBO]: A Read buffer is incomplete or undefined. All read buffers must specify attachment points that have images attached.\n");
+		  CSCI441::LogUtils::logError("[FBO]: A Read buffer is incomplete or undefined. All read buffers must specify attachment points that have images attached.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-		  fprintf(stderr, "[FBO]: All images must have the same number of multisample samples.\n");
+		  CSCI441::LogUtils::logError("[FBO]: All images must have the same number of multisample samples.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS :
-		  fprintf(stderr, "[FBO]: If a layered image is attached to one attachment, then all attachments must be layered attachments. The attached layers do not have to have the same number of layers, nor do the layers have to come from the same kind of texture.\n");
+		  CSCI441::LogUtils::logError("[FBO]: If a layered image is attached to one attachment, then all attachments must be layered attachments. The attached layers do not have to have the same number of layers, nor do the layers have to come from the same kind of texture.\n");
 		  break;
 
 		case GL_FRAMEBUFFER_UNSUPPORTED:
-		  fprintf(stderr, "[FBO]: Attempt to use an unsupported format combination!\n");
+		  CSCI441::LogUtils::logError("[FBO]: Attempt to use an unsupported format combination!\n");
 		  break;
 
 		default:
-		  fprintf(stderr, "[FBO]: Unknown error while attempting to create framebuffer object!\n");
+		  CSCI441::LogUtils::logError("[FBO]: Unknown error while attempting to create framebuffer object!\n");
 		  break;
 		}
 	}
